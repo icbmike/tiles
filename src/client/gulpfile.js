1690ef4996ee2f6090fdbed995cb48f0d2573ff2
Mike Little <mike@icbmike.com>
2015-09-14 20:18:37 +1000

    fix build task

--- src/client/gulpfile.js
+++ src/client/gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
 	browserify = require('gulp-browserify'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename');
 
 gulp.task('build', function(){
 	gulp.src('app.js')
 		.pipe(browserify({}))
 		.pipe(uglify())
-		.pipe(gulp.dest('./build/app.min.js'))
+		.pipe(rename('app.min.js'))
+		.pipe(gulp.dest('./build'))
 });
